9f0a098879878dffed8dddd1a9716dff52afc451
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2026-03-27 04:46:12 -0600

    feat: add screen services in veterinarian services

--- src/features/dashboard-cliente/domain/dtos/request/create-cita.request.ts
+++ src/features/dashboard-cliente/domain/dtos/request/create-cita.request.ts
@@ -2,6 +2,8 @@ export interface CreateCitaRequest {
   id_user: number
   id_mascota: number
   id_servicio: number
+  id_veterinario?: number
+  id_agenda?: number
   fecha: string
   observaciones_cliente?: string
 }
