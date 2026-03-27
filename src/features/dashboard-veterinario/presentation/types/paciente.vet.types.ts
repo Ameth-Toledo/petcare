e67d800c866d83c8f7fecf51df64af383c95214c
FabricioPRZ <233394@ids.upchiapas.edu.mx>
2026-03-27 02:51:44 -0600

    feat: dashboard-veterinario conectado

--- src/features/dashboard-veterinario/presentation/types/paciente.vet.types.ts
+++ src/features/dashboard-veterinario/presentation/types/paciente.vet.types.ts
@@ -1,9 +1,15 @@
 export interface PacienteVetProps {
-  id: number
-  nombre: string
-  especie: string
-  raza: string | null
-  nombre_dueno: string
-  apellido_dueno: string
-  activo: boolean
+  id:              number
+  id_user:         number
+  nombre:          string
+  especie:         string
+  raza:            string | null
+  fecha_nacimiento: string | null
+  sexo:            string | null
+  peso:            number | null
+  activo:          boolean
+  nombre_dueno?:   string
+  apellido_dueno?: string
+  email_dueno?:    string
+  telefono_dueno?: string
 }
